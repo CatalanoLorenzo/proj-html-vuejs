369655a37ffc1d9578053e0c416c62dc11b73cf8
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-04-26 14:43:08 +0200

    add section add and sections main

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,13 +1,18 @@
 <script>
-export default{
+export default {
     name: 'PageMain',
     data() {
         return {
-            
+
         }
     },
 }
 </script>
 <template>
-
+    <main>
+        <section class="about"></section>
+        <section class="projects"></section>
+        <section class="services"></section>
+        <section class="blog"></section>
+    </main>
 </template>
